f4f8a0b63d86d8ba95d3848dbf07b984e4848433
SNinjo <70991369+SNinjo@users.noreply.github.com>
2023-04-15 12:56:24 +0800

    v1.0.7 refactor

--- index.js
+++ index.js
@@ -48,68 +48,77 @@ export async function fetchAutoRetry(url, parameter, times = 5, delay = 5000) {
 
 
 export default async function joFetch(url, parameter, config = {}) {
-    // initialize
-    const sleep = (time) => new Promise(resolve => setTimeout(resolve, time))
-    config.useError = config.useError ?? ((error) => { throw error })
-    config.loadingTime = config.loadingTime ?? 10000
-    config.retryTimes = config.retryTimes ?? 5
-    config.retryDelay = config.retryDelay ?? 5000
-    config.typeFrom = config.typeFrom ?? ''
-    config.typeTo = config.typeTo ?? ''
+    const initialize = (config) => {
+        config.useError = config.useError ?? ((error) => { throw error })
+        config.loadingTime = config.loadingTime ?? 10000
+        config.retryTimes = config.retryTimes ?? 5
+        config.retryDelay = config.retryDelay ?? 5000
+        config.typeFrom = config.typeFrom ?? ''
+        config.typeTo = config.typeTo ?? ''
+    }
 
-    // fetch in time
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => reject(new Error('fetch overtime')), config.loadingTime)
-        fetch(url, parameter)
-            .then(resolve)
-            .catch(reject)
-    })
-        // retry on failure
-        .catch(async (error) => {
-            if (config.retryTimes-- <= 0) return config.useError(error)
-
-            await sleep(config.retryDelay)
-            return joFetch(url, parameter, config)
+    const fetchInTime = async (url, parameter, time) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject(new Error('fetch overtime')), time)
+            fetch(url, parameter)
+                .then(resolve)
+                .catch(reject)
         })
-    
-    // process data type
-    switch (config.typeFrom) {
-        case 'gzip':
-            promise = promise
-                .then(response => response.blob())
-                .then(data => new Response(data.stream().pipeThrough( new DecompressionStream('gzip') )))
-            break
-
-        case '':
-        default:
-            break
     }
-    switch (config.typeTo) {
-        case 'text':
-            promise = promise
-                .then(response => response.text())
-            break
 
-        case 'json':
-            promise = promise
-                .then(response => response.json())
-            break
+    const sleep = (time) => new Promise(resolve => setTimeout(resolve, time))
+    const retryOnFailure = async (error) => {
+        if (config.retryTimes-- <= 0) return config.useError(error)
 
-        case 'blob':
-            promise = promise
-                .then(response => response.blob())
-            break
-
-        case 'html':
-            promise = promise
-                .then(response => response.text())
-                .then(strHtml => new DOMParser().parseFromString(strHtml, "text/html"))
-            break
-            
-        case '':
-        default:
-            break
+        await sleep(config.retryDelay)
+        return joFetch(url, parameter, config)
     }
+
+    const processDataType = (promise) => {
+        switch (config.typeFrom) {
+            case 'gzip':
+                promise = promise
+                    .then(response => response.blob())
+                    .then(data => new Response(data.stream().pipeThrough( new DecompressionStream('gzip') )))
+                break
     
-    return promise
+            case '':
+            default:
+                break
+        }
+        switch (config.typeTo) {
+            case 'text':
+                promise = promise
+                    .then(response => response.text())
+                break
+    
+            case 'json':
+                promise = promise
+                    .then(response => response.json())
+                break
+    
+            case 'blob':
+                promise = promise
+                    .then(response => response.blob())
+                break
+    
+            case 'html':
+                promise = promise
+                    .then(response => response.text())
+                    .then(strHtml => new DOMParser().parseFromString(strHtml, "text/html"))
+                break
+                
+            case '':
+            default:
+                break
+        }
+        return promise
+    }
+
+
+    initialize(config)
+    return processDataType(
+        fetchInTime(url, parameter, config.loadingTime)
+            .catch(retryOnFailure)
+    )
 }
